d95be885d93d63f6bc09d1ada7d65561d37242ec
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-04-14 23:00:26 +0200

    publish site

--- studio/vite.config.ts
+++ studio/vite.config.ts
@@ -6,25 +6,12 @@ import path from "path";
 const githubPages404Plugin = () => ({
   name: "github-pages-404",
   closeBundle() {
-    const indexPath = path.resolve(__dirname, "../docs/skins/index.html");
+    const indexPath = path.resolve(__dirname, "../docs/index.html");
     const notFoundPath = path.resolve(__dirname, "../docs/404.html");
     try {
-      let indexContent = readFileSync(indexPath, "utf-8");
-      const redirectScript = `
-  <script>
-    (function() {
-      var path = window.location.pathname;
-      if (!path.startsWith('/skins/')) {
-        var redirectPath = '/skins/' + path.replace(/^\\//, '').replace(/^skins\\//, '');
-        var query = window.location.search;
-        var hash = window.location.hash;
-        window.location.replace(redirectPath + query + hash);
-      }
-    })();
-  </script>`;
-      indexContent = indexContent.replace("</body>", redirectScript + "\n</body>");
+      const indexContent = readFileSync(indexPath, "utf-8");
       writeFileSync(notFoundPath, indexContent, "utf-8");
-      console.log("✓ Generated docs/404.html for /skins SPA routing");
+      console.log("Generated docs/404.html");
     } catch (error) {
       console.error("Error generating docs/404.html:", error);
     }
@@ -33,9 +20,9 @@ const githubPages404Plugin = () => ({
 
 export default defineConfig({
   plugins: [react(), githubPages404Plugin()],
-  base: "/skins/",
+  base: "./",
   build: {
-    outDir: "../docs/skins",
+    outDir: "../docs",
     emptyOutDir: true,
   },
 });
